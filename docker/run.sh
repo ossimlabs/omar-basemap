4be560d2f4fbe0a250a890e7973b3843d26a9c08
Garrett Potts <gpotts@radiantblue.com>
2018-10-08 16:17:31 -0400

    Testing nss wrapper

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+sed '/^omar/d' /etc/passwd > /tmp/passwd
+echo omar:x:$USER_ID:$GROUP_ID:Default Application User:$HOME:/sbin/nologin >> /tmp/passwd
+
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
 start-stop-daemon --start --pidfile ~/xvfb.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
 sleep 1
 
